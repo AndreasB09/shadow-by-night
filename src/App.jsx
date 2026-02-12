0abd0bfdd1fd2657cc0d76f09c6cb79fbbc7bcf9
andreasb09 <andreasb09.kodehode@gmail.com>
2026-02-12 06:01:51 +0100

    cleanup and ScrollToTop component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 import HomePage from "./pages/Home/HomePage";
 import ClanPage from "./pages/Clan/ClanPage";
 import Navbar from "./components/Navbar/Navbar";
+import ScrollToTop from "./components/ScrollToTop";
+// import { useEffect } from "react";
+// import { useLocation } from "react-router-dom";
 
 
 function App() {
+
   return (
     <Router>
+      <ScrollToTop />
       <div className="banner-container">
         <div className="banner"></div>
       </div>
